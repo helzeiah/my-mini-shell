d8850da80dfc3b5871510ef71da02a12fa87db71
helzeiah <hdoftw@gmail.com>
2025-10-26 18:38:36 -0400

    process func -- currently supports sequencing.

--- process.c
+++ process.c
@@ -1,18 +1,29 @@
-		char *args[vect_size(tokens) + 1];
-		for (unsigned int i = 0; i < vect_size(tokens); i++) {
-			args[i] = (char *)vect_get(tokens, i);
-		}
-		args[vect_size(tokens)] = NULL;
-		
-		pid_t pid = fork();
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+void process_command(vect_t *tokens) {
+  char *args[vect_size(tokens) + 1];
+  for (unsigned int i = 0; i < vect_size(tokens); i++) {
+    args[i] = (char *)vect_get(tokens, i);
+  }
+  args[vect_size(tokens)] = NULL;
 
-		if (pid == 0) {
-			// child process
-			execvp(args[0], args);
-			fprintf(stderr, "%s: command not found\n", args[0]);
-			exit(1);
-		} else {
-			// parent process
-			waitpid(pid, NULL, 0);
-		}
+  pid_t pid = fork();
 
+  if (pid < 0) {
+    perror("fork");
+    return;
+  }
+  if (pid == 0) {
+    // child process
+    execvp(args[0], args);
+    fprintf(stderr, "%s: command not found\n", args[0]);
+    exit(1);
+  } else {
+    // parent process
+    waitpid(pid, NULL, 0);
+  }
+}
